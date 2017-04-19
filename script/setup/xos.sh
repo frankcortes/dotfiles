48e7abc00a547606f45cc19b4fc0f84a6342b49a
Francisco Cortés <franklyn.bdn@gmail.com>
2017-04-19 16:42:28 +0200

    require password inmediately after sleep begins

--- script/setup/xos.sh
+++ script/setup/xos.sh
@@ -20,6 +20,10 @@ if [[ OSX ]]; then
   defaults write NSGlobalDomain AppleMetricUnits -bool true
   echo "English i18n with metrical system [DONE]"
 
+  # Require password immediately after sleep or screen saver begins
+  defaults write com.apple.screensaver askForPassword -int 1
+  defaults write com.apple.screensaver askForPasswordDelay -int 0
+
   # Save screenshots to ~/screenshots
   defaults write com.apple.screencapture location -string "${HOME}/screenshots"
   echo "screenshots will be redirected [DONE]"
